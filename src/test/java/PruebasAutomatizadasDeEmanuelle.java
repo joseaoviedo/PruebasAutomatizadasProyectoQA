3b8fd5db580a8401aed4f7659c19f634c8eb290d
Emanuelle <emanuellejs@hotmail.es>
2020-07-20 21:01:23 -0600

    Pruebas automatizadas terminadas (Emanuelle)

--- src/test/java/PruebasAutomatizadasDeEmanuelle.java
+++ src/test/java/PruebasAutomatizadasDeEmanuelle.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
@@ -20,7 +17,7 @@ import org.junit.*;
 
 import static org.junit.Assert.*;
 
-public class PruebasDeEmanuelle {
+public class PruebasAutomatizadasDeEmanuelle {
 
     Base base;
 
@@ -438,35 +435,127 @@ public class PruebasDeEmanuelle {
     }
 
     @Test
-    public void prueba16() {
+    public void prueba16() throws InterruptedException {
 
-        assertEquals(0, 0);
+        base.redirect("http://localhost:8080/admin/content/contentImages.html");
+        WebElement uploadElement = base.getDriver().findElement(By.className("input-file"));
+        uploadElement.sendKeys("C:\\Users\\emanu\\Downloads\\prueba_imagen_grande.png");
+        Thread.sleep(1000);
+        base.clickClass("btn-success");
+
+        Thread.sleep(1000);
+
+        assertEquals("prueba_imagen_grande.png",base.getText(By.cssSelector("html body.body div.sm div.container div.row div.span9 div.tabbable div.tab-content div#catalogue-section.tab-pane.active div.sm-ui-component div#isc_B.normal div#isc_C div#isc_3.sectionStack div#isc_4 div#isc_0 div#isc_1 div#isc_J.simpleTile div#isc_K table.normal tbody tr td.name nobr")));
+
+        Thread.sleep(1000);
+
+        try{
+            Actions act = new Actions(base.getDriver());
+            WebElement shop = base.getDriver().findElement((By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[1]/div/img")));
+            act.moveToElement(shop).click().build().perform();
+            base.acceptAlert();
+        }catch (Exception e){
+
+        }
+
 
     }
 
     @Test
     public void prueba17() {
 
-        assertEquals(0, 0);
+        base.redirect("http://localhost:8080/admin/content/contentImages.html");
+        WebElement uploadElement = base.getDriver().findElement(By.className("input-file"));
+
+
+        try{
+            uploadElement.sendKeys("C:\\Users\\emanu\\Desktop\\prueba.txt");
+            Thread.sleep(1000);
+            base.clickClass("btn-success");
+
+            Thread.sleep(1000);
+            assertNotEquals("prueba.txt",base.getText(By.cssSelector("html body.body div.sm div.container div.row div.span9 div.tabbable div.tab-content div#catalogue-section.tab-pane.active div.sm-ui-component div#isc_B.normal div#isc_C div#isc_3.sectionStack div#isc_4 div#isc_0 div#isc_1 div#isc_J.simpleTile div#isc_K table.normal tbody tr td.name nobr")));
+
+            Thread.sleep(1000);
+            Actions act = new Actions(base.getDriver());
+            WebElement shop = base.getDriver().findElement((By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[1]/div/img")));
+            act.moveToElement(shop).click().build().perform();
+            base.acceptAlert();
+        }catch (InvalidArgumentException | InterruptedException e){
+            assertTrue(false);
+        }
 
     }
+
+    @Test
+    public void prueba19() throws InterruptedException {
+
+        base.redirect("http://localhost:8080/admin/content/contentImages.html");
+        WebElement uploadElement = base.getDriver().findElement(By.className("input-file"));
+        uploadElement.sendKeys("C:\\Users\\emanu\\Downloads\\foto.png");
+        Thread.sleep(1000);
+        base.clickClass("btn-success");
+
+        Thread.sleep(1000);
+
+        assertEquals("foto.png",base.getText(By.cssSelector("html body.body div.sm div.container div.row div.span9 div.tabbable div.tab-content div#catalogue-section.tab-pane.active div.sm-ui-component div#isc_B.normal div#isc_C div#isc_3.sectionStack div#isc_4 div#isc_0 div#isc_1 div#isc_J.simpleTile div#isc_K table.normal tbody tr td.name nobr")));
+
+        Thread.sleep(1000);
+
+        Actions act = new Actions(base.getDriver());
+        WebElement shop = base.getDriver().findElement((By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[1]/div/img")));
+        act.moveToElement(shop).click().build().perform();
+        base.acceptAlert();
+
+    }
+    
     @Test
     public void prueba18() {
 
-        assertEquals(0, 0);
+        base.redirect("http://localhost:8080/admin/content/contentImages.html");
+        WebElement uploadElement = base.getDriver().findElement(By.className("input-file"));
+
+
+        try{
+            uploadElement.sendKeys("C:\\Users\\emanu\\Downloads\\!#$%&()=.png");
+            Thread.sleep(1000);
+            base.clickClass("btn-success");
+
+            Thread.sleep(1000);
+            assertEquals("!#$%&()=.png",base.getText(By.cssSelector("html body.body div.sm div.container div.row div.span9 div.tabbable div.tab-content div#catalogue-section.tab-pane.active div.sm-ui-component div#isc_B.normal div#isc_C div#isc_3.sectionStack div#isc_4 div#isc_0 div#isc_1 div#isc_J.simpleTile div#isc_K table.normal tbody tr td.name nobr")));
+
+            Thread.sleep(1000);
+            Actions act = new Actions(base.getDriver());
+            WebElement shop = base.getDriver().findElement((By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[1]/div/img")));
+            act.moveToElement(shop).click().build().perform();
+            base.acceptAlert();
+        }catch (InvalidArgumentException | InterruptedException e){
+            assertTrue(false);
+        }
+
 
     }
-    @Test
-    public void prueba19() {
-
-        assertEquals(0, 0);
-
-    }
 
     @Test
-    public void prueba20() {
+    public void prueba20() throws InterruptedException {
 
-        assertEquals(0, 0);
+        base.redirect("http://localhost:8080/admin/content/static/contentFiles.html");
+        WebElement uploadElement = base.getDriver().findElement(By.className("input-file"));
+        uploadElement.sendKeys("C:\\Users\\emanu\\Downloads\\foto.png");
+        Thread.sleep(1000);
+        base.clickClass("btn-success");
+
+        Thread.sleep(1000);
+
+        assertEquals("foto.png",base.getText(By.cssSelector("#isc_Ptable > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > nobr:nth-child(1)")));
+
+        Thread.sleep(1000);
+
+        Actions act = new Actions(base.getDriver());
+        WebElement shop = base.getDriver().findElement((By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div/div/div/div/div/div/div[2]/div/div[3]/div/div/table/tbody[2]/tr/td[4]/div/nobr/img")));
+        act.moveToElement(shop).click().build().perform();
+        Thread.sleep(1000);
+        base.acceptAlert();
 
     }
 
---
+++ src/test/java/PruebasUnitariasDeEmanuelle.java
@@ -0,0 +1,163 @@
+
+import org.junit.*;
+
+public class PruebasUnitariasDeEmanuelle {
+
+    /*
+    -------------------------------
+        Pruebas unitarias
+    -------------------------------
+
+     */
+
+    @Test
+    public void prueba1(){
+
+    }
+    
+    @Test
+    public void prueba2(){
+
+    }
+
+    @Test
+    public void prueba3(){
+
+    }
+
+    @Test
+    public void prueba4(){
+
+    }
+
+    @Test
+    public void prueba5(){
+
+    }
+
+    @Test
+    public void prueba6(){
+
+    }
+
+    @Test
+    public void prueba7(){
+
+    }
+
+    @Test
+    public void prueba8(){
+
+    }
+
+    @Test
+    public void prueba9(){
+
+    }
+
+    @Test
+    public void prueba10(){
+
+    }
+
+    @Test
+    public void prueba11(){
+
+    }
+
+    @Test
+    public void prueba12(){
+
+    }
+
+    @Test
+    public void prueba13(){
+
+    }
+
+    @Test
+    public void prueba14(){
+
+    }
+
+    @Test
+    public void prueba15(){
+
+    }
+
+    @Test
+    public void prueba16(){
+
+    }
+
+    @Test
+    public void prueba17(){
+
+    }
+
+    @Test
+    public void prueba18(){
+
+    }
+
+    @Test
+    public void prueba19(){
+
+    }
+
+    @Test
+    public void prueba20(){
+
+    }
+
+    @Test
+    public void prueba21(){
+
+    }
+
+    @Test
+    public void prueba22(){
+
+    }
+
+    @Test
+    public void prueba23(){
+
+    }
+
+    @Test
+    public void prueba24(){
+
+    }
+
+    @Test
+    public void prueba25(){
+
+    }
+
+    @Test
+    public void prueba26(){
+
+    }
+
+    @Test
+    public void prueba27(){
+
+    }
+
+    @Test
+    public void prueba28(){
+
+    }
+
+    @Test
+    public void prueba29(){
+
+    }
+
+    @Test
+    public void prueba30(){
+
+    }
+
+}
